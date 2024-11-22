4fc3b3288d9c48c1ff9c952fe309a8fb1ec8c9d4
Shakil57375 <shakil57375@gmail.com>
2024-11-22 09:59:06 +0600

    updated

--- src/components/Transactions/Transaction.jsx
+++ src/components/Transactions/Transaction.jsx
@@ -1,18 +1,19 @@
 import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 export default function Transaction() {
-    return (
-        <li className="transaction income">
-            <p>Earned this month</p>
-            <div className="right">
-                <p>৳ 100</p>
-                <button className="link">
-                    delete
-                </button>
-                <button className="link">
-                   <FaEdit className="text-2xl"/>
-                </button>
-            </div>
-        </li>
-    );
+  return (
+    <li className="transaction income">
+      <p>Earned this month</p>
+      <div className="right">
+        <p>৳ 100</p>
+        <button className="link">
+          <MdDelete className="text-2xl text-red-500" />
+        </button>
+        <button className="link">
+          <FaEdit className="text-2xl text-blue-500" />
+        </button>
+      </div>
+    </li>
+  );
 }
